edb0070b095147f00d243aa01889684c3d290aa4
Nurbek0506 <nurbek050613@mail.ru>
2021-10-25 17:40:39 +0500

    Готовы версия проекта телебашни и адаптивная версия

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+function MenuActive() {
+    document.querySelector('header ul').classList.toggle('active');
+}
